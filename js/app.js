ab655805e5c9491c52a99947fcf510c7eeeaed5e
IvonReD <ivonne.reyes.dc@gmail.com>
2018-01-22 10:38:55 -0600

    Subiendo Proyecto Lyft

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  setTimeout(function() {
+        $("#splash").fadeOut(500);
+    },3000); /*oculta la pantalla inicial*/
+    setTimeout(function() {
+        $("#main").fadeIn(500);
+    },3000); /*Muestra la pantalla Principal*/
+
+
+});
