ae34cd4af5e7b46f24775a7e2b4944d79233c610
IvonReD <ivonne.reyes.dc@gmail.com>
2018-01-22 21:58:36 -0600

    Actualizando archivos html css

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
   setTimeout(function() {
-        $("#splash").fadeOut(500);
-    },3000); /*oculta la pantalla inicial*/
-    setTimeout(function() {
-        $("#main").fadeIn(500);
-    },3000); /*Muestra la pantalla Principal*/
+    $("#splash").fadeOut(500);
+  }, 3000); /*oculta la pantalla inicial*/
+  setTimeout(function() {
+    $("#main").fadeIn(500);
+  }, 3000); /*Muestra la pantalla Principal*/
 
+
+});
+$(function() {
+  $(".dropdown-menu li a").click(function() {
+    var text_selected = $(this).text();
+    $(".dropdown .user_selected").text(text_selected);
+  });
 
 });
