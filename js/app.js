e8599e59819d7000750699aacd1883dd2ad2b92e
IvonReD <ivonne.reyes.dc@gmail.com>
2018-01-23 04:53:35 -0600

    Actualizando archivos css html js y README

--- js/app.js
+++ js/app.js
@@ -5,13 +5,65 @@ $(document).ready(function() {
   setTimeout(function() {
     $("#main").fadeIn(500);
   }, 3000); /*Muestra la pantalla Principal*/
-
-
-});
-$(function() {
-  $(".dropdown-menu li a").click(function() {
-    var text_selected = $(this).text();
-    $(".dropdown .user_selected").text(text_selected);
-  });
+  $(".modal").modal();
 
 });
+
+
+
+/*+++++++++ Variables Globales++++++++*/
+var $inputPhone = $("#input-phone");
+var $btnNext = $("#btnNext");
+var $inputCode = $("#input-code");
+var $inputValidate = $("#input2");
+var $reset = $("#reset");
+var $btnNext3 = $("#btnNext3");
+
+
+/*+++++++++ Validando Numero Telefonico (pag2)++++++++*/
+/* --habilitar botón --*/
+$inputPhone.keyup(function() {
+  if ($(this).val().trim().length === 10) {
+    $btnNext.removeAttr("disabled");
+  } else {
+    $btnNext.attr("disabled", true);
+  }
+});
+
+
+
+$btnNext.click(function() {
+  var code = Math.floor(Math.random() * 900) + 100;
+  alert("LAB-- " + code);
+  localStorage.labCode = code;
+  localStorage.telephone = $inputPhone.val();
+});
+
+/*+++++++++ Validando Codigo (pag3)++++++++*/
+$inputValidate.keyup(function() {
+  if ($(this).val().trim().length === 3) {
+    $btnNext3.removeAttr("disabled");
+  } else if ($(this).val().trim().length !== 3 || $(this).val().trim().length === "") {
+    $btnNext3.attr("disabled", true);
+  }
+});
+
+
+$reset.click(function() {
+  var code = Math.floor(Math.random() * 900) + 100;
+  alert("LAB-- " + code);
+});
+
+
+/*+++++++++ Validando Nombre/Email ++++++++*/
+
+
+function validateDate() {
+  var $formDate = $("#btnDate");
+
+  if ($(this).val().trim().length > 0) {
+    $formDate.removeAttr("disabled");
+  } else {
+    $formDate.attr("disabled", true);
+  }
+}
